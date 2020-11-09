c8a10f9776b087d621903c47739b279ffa877535
dphoanghiep1998 <dinhphuhoanghiep_t63@hus.edu.vn>
2020-11-09 22:25:36 +0700

    update query

--- models/repository/branchDAO/BranchQuery.php
+++ models/repository/branchDAO/BranchQuery.php
@@ -4,4 +4,7 @@
 class BranchQuery
 {
     const SELECT_BY_ID = "SELECT * FROM branch WHERE id = '%s'";
+    const DELETE_BRANCH = "DELETE FROM branch WHERE '%s'";
+    const INSERT_BRANCH = "INSERT INTO branch(id, name) VALUES ('%s','%s')";
+    
 }
--- models/repository/taskDAO/TaskQuery.php
+++ models/repository/taskDAO/TaskQuery.php
@@ -3,4 +3,8 @@ class TaskQuery
 {
 
     const SELECT_BY_ID = "SELECT * FROM task WHERE id = '%s'";
+    const SELECT_TASK = "SELECT * FROM task WHERE '%s'";
+	const INSERT_TASK = "INSERT INTO task(id, phase_id, name, description,deadline) VALUES ('%s','%s','%s','%s','%s')";
+	const UPDATE_TASK = "UPDATE task SET id='%s',phae_id='%s',name='%s',description='%s',deadline='%s' WHERE '%s'";
+	const DELETE_TASK = "DELETE FROM task WHERE '%s'";
 }
