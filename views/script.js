46a699f0b5bb774acd269d9d9a7fa68d4930eb13
duyhiep2519 <duyhiep2519@gmail.com>
2020-11-23 21:18:56 +0700

    student,teacher

---
+++ views/script.js
@@ -0,0 +1,11 @@
+// handle message
+const sendMessage = (event) => {
+  event.preventDefault();
+  const value = document.getElementById("type_text").value;
+  if (value) {
+    document.getElementById(
+      "ul-message"
+    ).innerHTML += `<li id="li-message" >${value} <i class="fas fa-user-tie list-group-item"></i></li>`;
+    document.getElementById("type_text").value = "";
+  }
+};
