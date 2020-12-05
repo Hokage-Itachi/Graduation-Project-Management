b485f8888d292a391fb6530ee372432fdd56beab
thanhcute123 <thuthanhvu77@gmail.com>
2020-12-06 00:34:29 +0700

    change file script.js

--- views/script.js
+++ views/script.js
@@ -1,16 +1,4 @@
- <<<<<<< HEAD
-// handle message
-const sendMessage = (event) => {
-  event.preventDefault();
-  const value = document.getElementById("type_text").value;
-  if (value) {
-    document.getElementById(
-      "ul-message"
-    ).innerHTML += `<li id="li-message" >${value} <i class="fas fa-user-tie list-group-item"></i></li>`;
-    document.getElementById("type_text").value = "";
-  }
-};
-=======
+
 // handle message
 const sendMessage = (event) => {
   event.preventDefault();
